6f074245e1a455fb34a8e24e8d1054fbf9099a08
Sf-Shohag71 <shakhfarid71@gmail.com>
2024-01-08 15:11:47 +0600

    toggle form using previous and next button

--- script.js
+++ script.js
@@ -7,5 +7,23 @@ let currentStep = formSteps.findIndex((step) => {
 });
 if (currentStep < 0) {
   currentStep = 0;
-  formSteps[currentStep].classList.add("active");
+  // formSteps[currentStep].classList.add("active");
+  showCurrenStep();
 }
+
+multiStepForm.addEventListener("click", (e) => {
+  if (e.target.matches("[data-next]")) {
+    currentStep += 1;
+  } else if (e.target.matches("[data-previous]")) {
+    currentStep -= 1;
+  }
+  showCurrenStep();
+});
+
+function showCurrenStep(){
+  formSteps.forEach((step, index) => {
+    step.classList.toggle("active", index === currentStep);
+    console.log(step, index);
+  });
+  
+};
