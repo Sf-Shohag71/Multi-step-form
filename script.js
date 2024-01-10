38774170cb9e033015b757bdc7398d072a4157c1
Sf-Shohag71 <shakhfarid71@gmail.com>
2024-01-10 20:36:11 +0600

    completed multistep form

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-const multiStepForm = document.querySelector("[data-multi-step]");
-const formSteps = [...multiStepForm.querySelectorAll("[data-step]")];
-// console.log(formSteps);
+const multiStepForm = document.querySelector("[data-multi-step]")
+const formSteps = [...multiStepForm.querySelectorAll("[data-step]")]
+let currentStep = formSteps.findIndex(step => {
+  return step.classList.contains("active")
+})
 
-let currentStep = formSteps.findIndex((step) => {
-  return step.classList.contains("active");
-});
 if (currentStep < 0) {
-  currentStep = 0;
-  // formSteps[currentStep].classList.add("active");
-  showCurrenStep();
+  currentStep = 0
+  showCurrentStep()
 }
 
-multiStepForm.addEventListener("click", (e) => {
-  let incrementor;
+multiStepForm.addEventListener("click", e => {
+  let incrementor
   if (e.target.matches("[data-next]")) {
-    incrementor = 1;
+    incrementor = 1
   } else if (e.target.matches("[data-previous]")) {
-    incrementor = -1;
+    incrementor = -1
   }
 
-  if (incrementor == null) return;
-  const inputs = [...formSteps[currentStep].querySelectorAll("input")];
-  const allValid = inputs.every((input) => input.reportValidity());
-  console.log(allValid);
+  if (incrementor == null) return
+
+  const inputs = [...formSteps[currentStep].querySelectorAll("input")]
+  const allValid = inputs.every(input => input.reportValidity())
   if (allValid) {
-    currentStep += incrementor;
-    showCurrenStep();
+    currentStep += incrementor
+    showCurrentStep()
   }
-});
+})
 
-function showCurrenStep() {
+formSteps.forEach(step => {
+  step.addEventListener("animationend", e => {
+    formSteps[currentStep].classList.remove("hide")
+    e.target.classList.toggle("hide", !e.target.classList.contains("active"))
+  })
+})
+
+function showCurrentStep() {
   formSteps.forEach((step, index) => {
-    step.classList.toggle("active", index === currentStep);
-    // console.log(step, index);
-  });
+    step.classList.toggle("active", index === currentStep)
+  })
 }
